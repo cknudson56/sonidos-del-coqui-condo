8eb88333796dd39cf499ab1ea20c6e7773ce7dc4
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-12-14 11:41:54 -0600

    Finished Landing Page

--- form.php
+++ form.php
@@ -11,9 +11,9 @@ $form->processForm(
 	, 
 	'Contact Form' // Put your site name / form name here
 	, 
-	'christie.kosal@gmail.com' // Where will the form notification be sent?
+	'T.wendt@comcast.net' // Where will the form notification be sent?
 	, 
-	'christie.kosal@gmail.com' // This what the form FROM: address wil be, if the form submissions doesn't contain an email field
+	'T.wendt@comcast.net' // This what the form FROM: address wil be, if the form submissions doesn't contain an email field
 	
 );
 
